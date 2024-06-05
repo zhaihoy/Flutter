61b0d21ff84934246fdff0034c24cfae03106b75
翟洪元 <hongyuan3@staff.weibo.com>
2024-06-05 19:32:19 +0800

    请求网络权限设置

--- db/lib/weight/radius_img.dart
+++ db/lib/weight/radius_img.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class RadiusImg {
   static Widget get(String imgUrl, double imgW, {double? imgH, Color? shadowColor, double? elevation, double radius = 6.0, RoundedRectangleBorder? shape}) {
     shadowColor ??= Colors.transparent;
+    print("zhy"+imgUrl);
+
     return Card(
       //影音海报
       shape:shape?? RoundedRectangleBorder(
@@ -11,12 +14,13 @@ class RadiusImg {
       color: shadowColor,
       clipBehavior: Clip.antiAlias,
       elevation: elevation == null ? 0.0 : 5.0,
-      // child: Image.network(
-      //   imgUrl,
-      //   width: imgW,
-      //   height: imgH,
-      //   fit: imgH == null ? BoxFit.contain : BoxFit.cover,
-      // ),
+      child: CachedNetworkImage(
+        width: imgW,
+        height: imgH,
+        imageUrl: "http://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1355152571.6.webp",
+        placeholder: (context, url) => const CircularProgressIndicator(),
+        errorWidget: (context, url, error) => const Icon(Icons.error),
+      ),
     );
   }
 }
