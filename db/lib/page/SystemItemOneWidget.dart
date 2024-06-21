ed5c87ad5c1ba3aa855d4358b95ea599108a181e
翟洪元 <hongyuan3@staff.weibo.com>
2024-06-21 14:06:14 +0800

    优化代码

--- db/lib/page/SystemItemOneWidget.dart
+++ db/lib/page/SystemItemOneWidget.dart
@@ -1,50 +1,22 @@
 import 'package:flutter/material.dart';
 import '../bean/ChapterResponse.dart';
-import '../service/ApiService.dart';
 import '../weight/SystemItemCard.dart';
 
-class SystemItemOneWidget extends StatefulWidget {
-  List<Chapter> data;
+class SystemItemOneWidget extends StatelessWidget {
+  final List<Chapter> data;
 
-  SystemItemOneWidget(this.data, {super.key});
-
-  @override
-  State<SystemItemOneWidget> createState() => _SystemItemOneWidgetState();
-}
-
-class _SystemItemOneWidgetState extends State<SystemItemOneWidget> {
-  bool isLoading = true;
-  bool hasError = false;
-
-  @override
-  void initState() {
-    super.initState();
-    // _fetchItemData();
-  }
-  //
-  // Future<void> _fetchItemData() async {
-  //   if (widget.data.isEmpty) {
-  //       setState(() {
-  //         isLoading = false;
-  //       });
-  //   }
-  // }
+  const SystemItemOneWidget(this.data, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    // if (isLoading) {
-    //   return Center(child: CircularProgressIndicator());
-    // }
-    //
-    // if (hasError) {
-    //   return Center(child: Text('Error loading data.'));
-    // }
+    if (data.isEmpty) {
+      return const Center(child: Text('No data available.'));
+    }
 
     return ListView.builder(
-      itemCount: widget.data.length,
+      itemCount: data.length,
       itemBuilder: (context, index) {
-        return SystemItemCard(widget.data[index]);
-        // return Text(data[index]);
+        return SystemItemCard(data[index]);
       },
     );
   }
